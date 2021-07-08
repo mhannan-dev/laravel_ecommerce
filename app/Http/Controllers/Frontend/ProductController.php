ec4557f982dd2a473fb9104c146785cb350d527e
M Hannan <mdhannan.info@gmail.com>
2021-07-08 19:30:49 +0600

    Update

--- app/Http/Controllers/Frontend/ProductController.php
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -16,8 +16,8 @@ class ProductController extends Controller
         $categoryCount = Category::where(['slug' => $slug, 'status' => 1])->count();
         if ($categoryCount > 0) {
             $categoryDetails = Category::catDetails($slug);
-            //$categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1)->get()->toArray();
-            $categoryProducts = Product::with('brand')->whereIn('category_id', is_array($categoryDetails ['catIds']))->where('status', 1)->get()->toArray();
+           $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1)->get()->toArray();
+            // $categoryProducts = Product::with('brand')->whereIn('category_id', is_array($categoryDetails ['catIds']))->where('status', 1)->get()->toArray();
             return view('frontend.pages.products.listing', compact('categoryDetails', 'categoryProducts'));
         } else {
             abort(404);
